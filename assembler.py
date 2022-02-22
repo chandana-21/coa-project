0a8de457a2b11d95c674640a009b8cc0d831b0a3
chandana-21 <chandanakuntala21@gmail.com>
2022-02-22 12:44:08 +0530

    cp

--- assembler.py
+++ assembler.py
@@ -1,6 +1,6 @@
-opc_table = {} # stores opcodes occuring in the program in format { opcode : [binaryCodeForOpcode, size, inctructionClass] }
+opc_table = {} # stores opcodes occuring in the program in format { opcode : [bin_opcode, size, inctructionClass] }
 lab_table = {} # stores Label addresses { label : location_address }
-var_table = {} # stores variables in format { variable : [value,location_address,size] }
+var_table = {} # stores variables in format { variable : [value, location_address, size] }
 lit_table = {} # stores literals in format { literal : address}
 
 keywords = ['CLA','LAC','SAC','ADD','SUB','BRZ','BRN', 'BRP','INP', 'DSP', 'MUL', 'DIV', 'STP', 'DW']
@@ -47,6 +47,47 @@ def add_label(label, location_counter):
         lab_table[label] = location_counter
         return 3
 
+def add_opcode()
+
+def get_opcode(line):
+    opcodes = line.upper().split()
+
+    if opcodes.count('CLA') + opcodes.count('LAC') + opcodes.count('SAC') + opcodes.count('ADD') + opcodes.count('SUB') + opcodes.count('BRZ') + opcodes.count('BRN') + opcodes.count('BRP') + opcodes.count('INP') + opcodes.count('DSP') + opcodes.count('MUL') + opcodes.count('DIV') + opcodes.count('STP') + opcodes.count('DW') > 1:
+        return 0 # error declare opcodes only once
+
+    if opcodes.count('CLA') == 1:
+        add_opcode('CLA' )
+    elif opcodes.count('LAC') == 1:
+        add_opcode('LAC' )
+    elif opcodes.count('SAC') == 1:
+        add_opcode('SAC')
+    elif opcodes.count('ADD') == 1:
+        add_opcode('ADD')
+    elif opcodes.count('SUB') == 1:
+        add_opcode('SUB' )
+    elif opcodes.count('BRZ') == 1:
+        add_opcode('BRZ' )
+    elif opcodes.count('BRN') == 1:
+        add_opcode('BRN' )
+    elif opcodes.count('BRP') == 1:
+        add_opcode('BRP' )
+    elif opcodes.count('INP') == 1:
+        add_opcode('INP' )
+    elif opcodes.count('DSP') == 1:
+        add_opcode('DSP' )
+    elif opcodes.count('MUL') == 1:
+        add_opcode('MUL' )
+    elif opcodes.count('DIV') == 1:
+        add_opcode('DIV' )
+    elif opcodes.count('STP') == 1:
+        add_opcode('STP')
+    else:
+        pass
+    
+    
+    
+    
+
 def pass_one():
     location_counter = 0;
 
@@ -61,6 +102,9 @@ def pass_one():
                 print("Error!",label, " has been declared as a Variable already!")
             if i==2:
                 print("Error!",label, " is a reserved keyword, can't be used as a label")
+        
+        pass_one_code.append(roline)
+        get_opcode(roline)
         
 
 
@@ -88,7 +132,7 @@ def pass_one():
 
 
 # OPC_TABLE["CLA"] = ["0000",0]
-# OPC_TABLE["LAC"] = ["0001",1]
+# OPC_TABLE["LA"] = ["0001",1]
 # OPC_TABLE["SAC"] = ["0010",1]
 # OPC_TABLE["ADD"] = ["0011",1]
 # OPC_TABLE["SUB"] = ["0100",1]
@@ -101,7 +145,7 @@ def pass_one():
 # OPC_TABLE["DIV"] = ["1011",1]
 # OPC_TABLE["STP"] = ["1100",0]
 # OPC_TABLE["CLA"] = ["0000" ]
-# OPC_TABLE["LAC"] = ["0001" ]
+# OPC_TABLE["LA"] = ["0001" ]
 # OPC_TABLE["SAC"] = ["0010" ]
 # OPC_TABLE["ADD"] = ["0011" ]
 # OPC_TABLE["SUB"] = ["0100" ]
