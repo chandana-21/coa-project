70dfa7bcd7e342ea35860166f2af02b792e24681
chandana-21 <chandanakuntala21@gmail.com>
2022-02-23 17:09:48 +0530

    cp

--- assembler.py
+++ assembler.py
@@ -1,6 +1,6 @@
 opc_table = {} # stores opcodes of the program like { opcode : bin_opcode }
 lab_table = {} # stores Label addresses like { label : location_address }
-var_table = {} # stores variables like { variable : [value, location_address, size] }
+var_table = {} # stores variables like { variable : [value, location_address] }
 lit_table = {} # stores literals like { literal : address}
 
 keywords = ['CLA','LAC','SAC','ADD','SUB','BRZ','BRN', 'BRP','INP', 'DSP', 'MUL', 'DIV', 'STP', 'DEC']
@@ -8,7 +8,7 @@ keywords = ['CLA','LAC','SAC','ADD','SUB','BRZ','BRN', 'BRP','INP', 'DSP', 'MUL'
 code = ['CLA',
         'INP A ',
         'INP B',
-        'LAC A; checking comments',
+        'LAC A ; checking comments',
         "SUB '=3'",
         'BRN L2',
         'DSP L1',
@@ -19,6 +19,7 @@ code = ['CLA',
         'BRZ L2',
         'L2 ~ STP',
         'A DEC ',
+        # 'L2 ~ STP',
         'B DEC 125']
 
 pass_one_code = []
@@ -70,29 +71,19 @@ def add_label(label, location_counter):
         return 3
 
 
+def label_error(num,label):
+    if num==0:
+        print("Error!",label, " has been declared as a Label already!")
+    if num==1:
+        print("Error!",label, " has been declared as a Variable already!")
+    if num==2:
+        print("Error!",label, " is a Reserved Keyword, can't be used as a Label")
+        
+
+
 def add_opcode(opcode, bin_opcode):
     if list(opc_table.keys()).count(opcode)==0:
         opc_table[opcode] = [bin_opcode]
-
-
-def get_variable(line):
-    value = 0
-    size = 12
-    parts = line.upper().split()
-    i = parts.count("DEC")
-    if i==0:
-        return None, None, None
-    elif i>1 or len(parts)>3:
-        return 0,0,0 #error
-    variable = parts[0].strip()
-    if variable.isdigit():
-        return 0,0,0 #error
-    if len(parts)==3:
-        value = parts[2].strip()
-        if not value.isdigit():
-            return 0,0,0 #error
-    value = int(value)
-    return variable, value, size   
 
 
 def get_opcode(line):
@@ -103,37 +94,101 @@ def get_opcode(line):
 
     if opcodes.count('CLA') == 1:
         add_opcode('CLA',"0000" )
+
     elif opcodes.count('LAC') == 1:
         add_opcode('LAC', "0001")
+
     elif opcodes.count('SAC') == 1:
         add_opcode('SAC', "0010")
+
     elif opcodes.count('ADD') == 1:
         add_opcode('ADD', "0011")
+
     elif opcodes.count('SUB') == 1:
         add_opcode('SUB', "0100")
     elif opcodes.count('BRZ') == 1:
         add_opcode('BRZ', "0101")
+
     elif opcodes.count('BRN') == 1:
         add_opcode('BRN', "0110")
+
     elif opcodes.count('BRP') == 1:
         add_opcode('BRP', "0111")
+
     elif opcodes.count('INP') == 1:
         add_opcode('INP', "1000")
+
     elif opcodes.count('DSP') == 1:
         add_opcode('DSP', "1001")
+
     elif opcodes.count('MUL') == 1:
         add_opcode('MUL', "1010")
+
     elif opcodes.count('DIV') == 1:
         add_opcode('DIV', "1011")
+
     elif opcodes.count('STP') == 1:
         add_opcode('STP',"1100")
     else:
-        variable, value, size = get_variable(line)
-        if variable==0 and value==0 and size==0:
-            return "error"
-        if variable==None:
-            return "error"
-        # To be continued 
+        pass
+        # variable, value = get_variable(line)#, size
+        # # if variable==0 and value==0 : #and size==0
+        # #     return "error"
+        # if variable==None:
+        #     return "error"
+        # num = add_variable(variable, value)#, size
+        # if num==0:
+        #     print("Error!",variable, " has been declared as a Label already!")
+        # if num==1:
+        #     print("Error!",variable, " has been declared as a Variable already!")
+        # if num==2:
+        #     print("Error!",variable, " is a Reserved Keyword, can't be used as a Variable")
+
+
+def get_variable(line):
+    value = 0
+    # size = 12
+    parts = line.upper().split()
+    i = parts.count('DEC')
+    # print(i)#test
+    # print(parts)#test
+    if i==0:
+        return None, None #error
+    if i>1 or len(parts)>3:
+        return None, None #error
+    variable = parts[0].strip()
+    if variable.isdigit():
+        return None, None #error
+    if i==1 and len(parts)==3:
+        print("y")#test
+        value = parts[2].strip()
+        print(variable,value)#test
+        if not value.isdigit():
+            # print("n")#test
+            return None, None #error
+    # value = int(value)
+    return variable, value#, size   
+
+
+def add_variable(variable, value): #, size
+    if duplicate_label(variable):
+        return 0 #error
+    elif duplicate_variable(variable):
+        return 1 #error
+    elif variable in keywords :
+        return 2 #error
+    else:
+        lab_table[variable] = [value, '0']
+        return 3
+
+
+def variable_error(num,variable):
+    if num==0:
+        print("Error!",variable, " has been declared as a Label already!")
+    if num==1:
+        print("Error!",variable, " has been declared as a Variable already!")
+    if num==2:
+        print("Error!",variable, " is a Reserved Keyword, can't be used as a Variable")
 
 
 def pass_one():
@@ -146,21 +201,24 @@ def pass_one():
 
         if label != ' ':
             i = add_label(label, location_counter)
-            if i==0:
-                print("Error! Label has already been defined once")
-            if i==1:
-                print("Error!",label, " has been declared as a Variable already!")
-            if i==2:
-                print("Error!",label, " is a reserved keyword, can't be used as a label")
-        
+            label_error(i,label)
+            
         pass_one_code.append(roline)
-        get_opcode(roline)
+        get_opcode(roline) # works till here 
+
+        variable, value = get_variable(roline)
+        if variable != None:
+            print("yes",variable,value)#test
+            i = add_variable(variable, value)
+            variable_error(i,variable)
+        
 
     return pass_one_code
 
-# code = pass_one()
+code = pass_one()
 # print(code)
 # print(opc_table)
+print(var_table)
 
 
 
